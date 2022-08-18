86cbf9ce582d96d80356ed028c2620b26091a63d
chenyue <chenyue.com>
2022-08-18 23:19:59 +0800

    babel编译js代码

---
+++ homeworks_html/lib/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+console.log(1); // 定义变量
+
+var name = "xiaoming"; // 导出变量
+
+var _default = name; // // 导出匿名函数
+// export default function(){};
+// // 导出class类
+// class Person{};
+// const name = "xiaoming";
+// export {Person,name};
+// export default function(){};
+
+exports["default"] = _default;
---
+++ homeworks_html/src/index.js
@@ -0,0 +1,16 @@
+console.log(1);
+
+// 定义变量
+const name = "xiaoming";
+// 导出变量
+export default name;
+
+// // 导出匿名函数
+// export default function(){};
+
+// // 导出class类
+// class Person{};
+// const name = "xiaoming";
+
+// export {Person,name};
+// export default function(){};
