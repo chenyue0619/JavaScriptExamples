c60cb97f1bb5a69c18510f7b7f6f3095712b2c92
chenyue <chenyue.com>
2022-08-22 01:09:48 +0800

    cookie的封装

---
+++ homeworks_html/js/cookie.js
@@ -0,0 +1,42 @@
+const set = (name,value,{maxAge,domain,path,secure} = {}) => {
+    let cookieType = `${encodeURIComponent(name)}=${encodeURIComponent(value)}`;
+    if(maxAge){
+        cookieType += `;maxAge=${maxAge}`;
+    }
+    if(domain){
+        cookieType += `;domain=${domain}`;
+    }
+    if(path){
+        cookieType += `;path=${path}`;
+
+    }
+    if(secure){
+        cookieType += `;secure=${secure}`;
+
+    }
+    document.cookie = cookieType;
+    return cookieType;
+}
+
+const get = (name) => {
+    name = `${encodeURIComponent(name)}`;
+    let cookieText = document.cookie.split(";");
+    for(let item of cookieText){
+        let [cookieName,cookieValue] = item.split("=");
+        if(cookieName === name){
+            return encodeURIComponent(cookieValue);
+        }
+    }
+    return;
+}
+
+// 根据name,domain,path三者相同才是同一个cookie
+// 将maxAge = 0/负数，这个时候可以删除cookie
+const remove = (name,{domain,cookie}) = {
+    set(name,"",{
+        domain,
+        path,
+        maxAge:-1
+    })
+}
+export{set,get,remove};
